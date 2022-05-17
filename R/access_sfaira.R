87b5c661d0e0ca5780a659b9200e4c3a217be690
Alexander Dietrich <adietrich@norm.exbio.wzw.tum.de>
2022-05-17 16:05:35 +0200

    extend examples

--- R/access_sfaira.R
+++ R/access_sfaira.R
@@ -12,7 +12,7 @@
 #' @export
 #'
 #' @examples
-#' sfaira_list <- setup_sfaira(basedir=tempdir())
+#' setup_list <- setup_sfaira(basedir=tempdir())
 setup_sfaira <- function(basedir){
   tryCatch({
     # create conda environment with sfaira
@@ -183,6 +183,7 @@ download_sfaira_multiple <- function(setup_list, organisms=NULL, tissues=NULL, a
 #' @export
 #'
 #' @examples
+#' setup_list <- setup_sfaira(basedir=tempdir())
 #' all_datasets <- sfaira_overview(setup_list)
 sfaira_overview <- function(setup_list){
   
