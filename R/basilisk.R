4286bf5ac697952c4cb69f35fb17166c7a90963f
Alexander Dietrich <adietrich@norm.exbio.wzw.tum.de>
2022-05-19 14:45:19 +0200

    try tables v3.6.1

--- R/basilisk.R
+++ R/basilisk.R
@@ -2,16 +2,17 @@
 # dependencies for sfaira and anndata0.7.6 (taken from zellkonverter package)
 SimBu_env <- basilisk::BasiliskEnvironment(envname="SimBu_env_0_99_4",
                                             pkgname="SimBu",
-                                            packages=c('python==3.9', 
-                                                       "anndata==0.7.6",
-                                                       "h5py==3.2.1",
-                                                       "hdf5==1.10.6",
-                                                       "natsort==7.1.1",
-                                                       "numpy==1.20.2",
-                                                       "packaging==20.9",
-                                                       "pandas==1.2.4",
-                                                       "scipy==1.6.3",   
-                                                       "sqlite==3.35.5"),
-                                            pip = c('sfaira==0.3.12')
+                                           packages=c('python==3.9', 
+                                                      "anndata==0.7.6",
+                                                      "h5py==3.2.1",
+                                                      "hdf5==1.10.6",
+                                                      "natsort==7.1.1",
+                                                      "numpy==1.20.2",
+                                                      "packaging==20.9",
+                                                      "pandas==1.2.4",
+                                                      "scipy==1.6.3",   
+                                                      "sqlite==3.35.5"),
+                                           pip = c('sfaira==0.3.12',
+                                                   'tables=3.6.1')
 )
 
